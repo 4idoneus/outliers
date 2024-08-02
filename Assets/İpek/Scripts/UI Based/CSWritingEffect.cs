6ead18a16d067f46d6afa3d1f7bcb33eff594cad
4idoneus <ipeknaz.sipahi@gmail.com>
2024-08-02 11:52:53 +0300

    First Half of Timeline/Dialogue

---
+++ Assets/İpek/Scripts/UI Based/CSWritingEffect.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CTWritingEffect : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (textComponent.text == lines[index]) 
+            {
+                nextLine();
+            }
+            else 
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue() 
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+    }
+    IEnumerator TypeLine()
+    {
+        // Type each character 1 by 1
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void nextLine()
+    {
+        if(index < lines.Length - 1) 
+        {
+        index++;
+            textComponent.text = string.Empty;
+            StartCoroutine (TypeLine());
+        }
+        else
+        {
+            Invoke("GoToNextScene", 0.2f);
+        }
+    }
+    void GoToNextScene()
+    {
+        // Replace "NextSceneName" with the actual name of the scene you want to load
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
